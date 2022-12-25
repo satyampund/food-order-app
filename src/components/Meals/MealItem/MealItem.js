c5f7ae7fd0aafda687d06509607f7b306e9419ce
Satyam Pund <pundsatyam@gmail.com>
2022-12-25 15:49:58 +0530

    feat: badge update

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './MealItem.css';
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
   return (
     <li className="meal">
       <div>
@@ -12,7 +24,7 @@ const MealItem = (props) => {
         <div className="price">{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './MealItemForm.css';
 import Input from './../../UI/Input';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         lable="Amount"
         input={{ id: 'Amount', type: 'number', min: '1', max: '5', step: '1', defaultValue: '1' }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter the valid amount - 1 to 5</p>}
     </form>
   );
 };
